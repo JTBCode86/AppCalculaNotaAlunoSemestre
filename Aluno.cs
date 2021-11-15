09b38ddd1f699e44181b112d58fb3a010e2fb703
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-11-14 22:08:43 -0300

    Adicionar arquivos de projeto.

---
+++ Aluno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCalculaNotaAlunoSemestre
+{
+    class Aluno
+    {
+        public string nome;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+        
+        public double calculaNotaFinal() 
+        {
+            return nota1 + nota2 + nota3;
+        }
+
+        public bool aprovado()
+        {
+            if (calculaNotaFinal()>60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double notaRestante() 
+        {
+            if (aprovado())
+            {
+                return 0;
+            }
+            else
+            {
+                return 60.0 - calculaNotaFinal();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace AppCalculaNotaAlunoSemestre
+{
+    /// <summary>
+    /// Exercicios - Fixação - POO - Classes
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            Console.WriteLine("Nome do aluno: ");
+            aluno.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas: ");
+            aluno.nota1 = double.Parse(Console.ReadLine());
+            aluno.nota2 = double.Parse(Console.ReadLine());
+            aluno.nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("NOTA FINAL = " + aluno.calculaNotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            
+            if (aluno.aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + aluno.notaRestante().ToString("F2", CultureInfo.InvariantCulture)+ " PONTOS");
+            }
+            Console.ReadLine();
+        }
+    }
+}
